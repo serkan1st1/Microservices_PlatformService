c20f21f516e39ed6d9bbabe13e593ec4a28da648
Serkan Topal <serkan98topal@gmail.com>
2023-10-19 02:08:23 +0300

    updating our Platform Service to use SQL Server

--- Startup.cs
+++ Startup.cs
@@ -19,18 +19,32 @@ namespace PlatformService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        private readonly IWebHostEnvironment _env;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env=env;
         }
 
-        public IConfiguration Configuration { get; }
-
+      
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(opt =>
-                    opt.UseInMemoryDatabase("InMem"));
+            if(_env.IsProduction())
+            {
+                Console.WriteLine("Using SqlServer Db");
+                services.AddDbContext<AppDbContext>(opt=>
+                opt.UseSqlServer(Configuration.GetConnectionString("PlatformsConn")));
+            }
+            else
+            {
+                Console.WriteLine("Using InMem Db");
+                services.AddDbContext<AppDbContext>(opt =>
+                                opt.UseInMemoryDatabase("InMem"));
+            }
+         
 
             services.AddScoped<IPlatformRepo,PlatformRepo>();      
 
@@ -65,7 +79,7 @@ namespace PlatformService
                 endpoints.MapControllers();
             });
 
-            PrepDb.PrepPopulation(app);
+            PrepDb.PrepPopulation(app, env.IsProduction());
         }
     }
 }
